be5373f9914ccfa941e4f38ea5f9d961b0b33cc1
Stefano Verna <stefano.verna@welaika.com>
2012-09-04 10:10:26 +0200

    Moved vimfiles to dotfiles

--- todo.txt/aliases.zsh
+++ todo.txt/aliases.zsh
@@ -1,5 +1,5 @@
 alias n="t ls +next"
 
 redpomo_add_urls() {
-  cat - | sed -E "s/^(.*#([0-9]+).*@cantiere.*$)/\1 $(tput setaf 0) — https:\/\/project.cantierecreativo.net\/issues\/\2/ ; s/^(.*#([0-9]+).*@welaika.*$)/\1 $(tput setaf 0) — http:\/\/code.welaika.com\/issues\/\2/"
+  cat - | sed -E "s/^(.*#([0-9]+).*@cantiere.*$)/\1 $(tput setaf 0) — https:\/\/project.cantierecreativo.net\/issues\/\2/ ; s/^(.*#([0-9]+).*@welaika.*$)/\1 $(tput setaf 0) — http:\/\/code.welaika.com\/issues\/\2/ ; s/^(.*#([0-9]+).*@agavee.*$)/\1 $(tput setaf 0) — http:\/\/projects.agavee.com\/issues\/\2/"
 }
