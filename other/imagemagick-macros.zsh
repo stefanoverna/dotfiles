7d46ff93db7f8cd86b5dd5b7a4c56f8a5d249225
Stefano Verna <stefano.verna@welaika.com>
2012-09-16 23:31:26 +0200

    Cleanup

--- other/imagemagick-macros.zsh
+++ other/imagemagick-macros.zsh
@@ -7,3 +7,15 @@ function convert-to-alpha() {
     convert \( $1 -channel RGB -separate \) \( $1 -fx intensity -negate \) -channel RGBA -combine $2
   fi
 }
+
+function fill-with-white() {
+  if [ $# -eq 0 ]; then
+    echo "usage: $0 [source] [destination]"
+  elif [ $# -eq 1 ]; then
+    convert $1 \( -clone 0 -fill white -draw "color 0,0 reset" \) -compose atop -composite $1
+  else
+    convert $1 \( -clone 0 -fill white -draw "color 0,0 reset" \) -compose atop -composite $2
+  fi
+}
+
+
