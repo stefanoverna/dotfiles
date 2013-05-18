6c06a20371d58216e36ecc930ea5e39d2070debf
Stefano Verna <stefano.verna@welaika.com>
2013-05-18 09:30:37 +0200

    html2slim takes STDIN, zsh prompt theme

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,4 @@
-export PATH="./bin/stubs:$HOME/bin:$HOME/.rbenv/shims:/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export GOPATH=$HOME/code/go
+export PATH="./bin/stubs:$GOPATH/bin:$HOME/bin:$HOME/.rbenv/shims:/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,69 +1,8 @@
+setopt promptsubst
+
 autoload colors && colors
+autoload -U promptinit && promptinit
+prompt grb
 
-git_wip() {
-  st=$(git log --format="%s" -1 | tr '[:upper:]' '[:lower:]')
-  if [[ $st == "wip" ]]; then
-    echo "— %{$fg_bold[magenta]%}WIP!%{$reset_color%}"
-  else
-    echo ""
-  fi
-}
+precmd() { _z --add "$(pwd -P)" }
 
-git_branch() {
-  ref=$(git symbolic-ref HEAD)
-  branch="${ref#refs/heads/}"
-  st=$(git diff --quiet && git diff --cached --quiet && test -z "$(git ls-files --others --exclude-standard)")
-  if test "$?" = 0; then
-    echo "%{$fg_bold[green]%}$branch%{$reset_color%}"
-  else
-    echo "%{$fg_bold[red]%}$branch!%{$reset_color%}"
-  fi
-}
-
-git_need_push () {
-  has_upstream=$(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD))
-  if [ -n "$has_upstream" ]; then
-    st=$(git cherry -v @{upstream})
-    if [[ $st == "" ]]; then
-      echo " "
-    else
-      echo " with %{$fg_bold[blue]%}unpushed%{$reset_color%} "
-    fi
-  fi
-}
-
-is_git_repo() {
-  [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1
-}
-
-git_infos() {
-  if is_git_repo; then
-    echo "on $(git_branch)$(git_need_push)$(git_wip)"
-  fi
-}
-
-rbenv_infos() {
-  if $(which rbenv &> /dev/null); then
-    version=$(rbenv version | awk '{print $1}')
-    gemset=$(rbenv gemset active 2&>/dev/null | head -n1)
-    if [[ "$gemset" != "" ]]; then
-      gemset="%{$fg_bold[yellow]%} ($gemset)%{$reset_color%}"
-    fi
-    echo "%{$fg_bold[green]%}$version$gemset%{$reset_color%}"
-  fi
-}
-
-directory_name(){
-  echo "%{$fg_bold[blue]%}%1/%\/%{$reset_color%}"
-}
-
-export PROMPT=$'\n$(rbenv_infos) in $(directory_name) $(git_infos)\n%{$fg[green]%}›%{$reset_color%} '
-set_prompt () {
-  export RPROMPT=""
-}
-
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  _z --add "$(pwd -P)"
-  set_prompt
-}
