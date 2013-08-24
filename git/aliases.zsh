262b9cd02a6bd4decb6aa6d777913d4fc0487b8e
Stefano Verna <stefano.verna@welaika.com>
2013-08-24 14:46:13 +0200

    Git aliases
    - Added zsh aliases for git
    - Made git aliases more descriptive

---
+++ git/aliases.zsh
@@ -0,0 +1,22 @@
+alias g='git'
+
+alias gs='g status-diff'
+alias gc='g commit'
+alias gp='g pull-rebase'
+alias gd='g diff'
+alias gr='g remote'
+
+alias ga='g add'
+alias gaa='g add-remove-all'
+alias gai='g add-interactive'
+
+alias gru='g remove-untracked'
+alias grs='g remove-from-staging'
+
+alias grb='g rebase-interactive'
+alias grc='g amend'
+
+alias gl='g nice-oneline-log'
+alias gll='g nice-detail-log'
+alias gi='g summary && g info --no-config'
+
--- git/functions.zsh
+++ git/functions.zsh
@@ -71,3 +71,4 @@ function git() {
     command git "$@";
   fi;
 }
+
