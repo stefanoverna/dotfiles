09fea2d03d5247537d452da73a03882bab1dca0b
Stefano Verna <stefano.verna@welaika.com>
2013-08-25 22:35:35 +0200

    Heroku aliases

---
+++ heroku/aliases.zsh
@@ -0,0 +1,18 @@
+alias h='heroku'
+
+alias hr='heroku run'
+alias hrd='heroku run:detached'
+
+alias hlg='heroku logs'
+alias hlgt='heroku logs --tail'
+
+alias hmon!='heroku maintenance:on'
+alias hmoff='heroku maintenance:off'
+
+alias hc='heroku config'
+alias hca='heroku config:add'
+
+alias hpr='heroku ps:restart'
+alias hle= 'heroku labs:enable'
+alias ha='git config heroku.account'
+
