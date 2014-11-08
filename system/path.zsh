1d540b8daad386eacd31958e4562a31b8c391009
Stefano Verna <s.verna@cantierecreativo.net>
2014-11-08 16:08:52 +0100

    Fixes

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,5 @@
 export GOPATH=$HOME/code/go
-export PATH="./binstubs:./bin:/usr/local/heroku/bin:$GOPATH/bin:$HOME/bin:$HOME/.rbenv/shims:/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./binstubs:./bin:./node_modules/.bin:/usr/local/heroku/bin:$GOPATH/bin:$HOME/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export NVM_DIR="/Users/stefanoverna/.nvm"
 
