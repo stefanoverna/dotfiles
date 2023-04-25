172fe2b7946de3d09cbd4c17301b80905c01a500
Stefano Verna <s.verna@datocms.com>
2023-04-25 15:41:21 +0200

    Fix

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,6 @@
 export GOPATH=$HOME/code/go
 export GOROOT=/usr/local/opt/go/libexec
-export PATH="./binstubs:./bin:./node_modules/.bin:/usr/local/heroku/bin:$GOPATH/bin:$HOME/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/local/opt/go/libexec/bin:$PATH"
+export PATH="./binstubs:./bin:./node_modules/.bin:/usr/local/heroku/bin:$GOPATH/bin:$HOME/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/local/opt/go/libexec/bin:/opt/homebrew/opt/postgresql@11/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export NVM_DIR="$HOME/.nvm"
+export GPG_TTY=$(tty)
