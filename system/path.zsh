84e68ec0ea4e2a4c9b365093ce9207c1574022a7
Stefano Verna <s.verna@datocms.com>
2024-03-11 13:51:05 +0100

    Improvements

--- system/path.zsh
+++ system/path.zsh
@@ -1,6 +1,6 @@
 export GOPATH=$HOME/code/go
 export GOROOT=/usr/local/opt/go/libexec
-export PATH="./binstubs:./bin:./node_modules/.bin:/usr/local/heroku/bin:$GOPATH/bin:$HOME/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/local/opt/go/libexec/bin:/opt/homebrew/opt/postgresql@11/bin:$PATH"
+export PATH="./binstubs:./bin:./node_modules/.bin:/usr/local/heroku/bin:/opt/homebrew/opt/python/libexec/bin:$GOPATH/bin:$HOME/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/local/opt/go/libexec/bin:/opt/homebrew/opt/postgresql@15/bin:$HOME/.cargo/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export NVM_DIR="$HOME/.nvm"
 export GPG_TTY=$(tty)
